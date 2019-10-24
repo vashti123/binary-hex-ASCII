70a42cf8d55472b92e970a7491c105443587bf5b
vashti123 <31171100+vashti123@users.noreply.github.com>
2019-10-24 14:42:25 -0400

    Update converter.js
    This is my new code, it works! Thanks Mr.Smith.

--- Converter/converter.js
+++ Converter/converter.js
@@ -1,23 +1,25 @@
-var decimal = document.getElementById('dec').value
-var binary = document.getElementById('bin').value
-var hex = document.getElementById('hex').value
-var ascii = document.getElementById('ascii').value
-
-function convert() {
-
-  function convertDec() {
-    console.log(decimal);
-  }
-
-  function convertBin() {
-    console.log(binary.toString(2));
-  }
-
-  function convertHex() {
-    console.log(hex.toString(16));
-  }
-
-  function convertAscii() {
-  console.log(hex.toString(16));
+function binToHex() {
+  var number = document.getElementById('bin').value;
+  var hexa = parseInt(number, 2).toString(16).toUpperCase();
+  document.getElementById('hexa').value = hexa
+}
+function hexToBin() {
+  var number = document.getElementById('hexa').value;
+  var bin = parseInt(number, 16).toString(2).toUpperCase();
+  document.getElementById('bin').value = bin
+}
+function hexToASCII() {
+  var ascii = document.getElementById('hexa').value.toString();
+  for (var i = 0; i < hexa.length; i += 2)
+        str += String.fromCharCode(parseInt(hexa.substr(i, 2), 16));
+  document.getElementById('ascii').value = ascii
+}
+function asciiToHex() {
+  var number = [];
+  var str = document.getElementById('ascii').value;
+  for (var i = 0, l = str.length; i < l; i ++) {
+    var hexa = Number(str.charCodeAt(i)).toString(16);
+    number.push(hexa);
+  document.getElementById('hexa').value = number
   }
 }
